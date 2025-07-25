6902871913f7646da77d3374ff7cbd84bdb7393a
The Engineer <132500641+OmarAyman2030@users.noreply.github.com>
2025-07-25 16:47:19 +0300

    Add files via upload

---
+++ Services/EmailServices.cs
@@ -0,0 +1,34 @@
+﻿using MimeKit;
+using MimeKit.Text;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+
+
+namespace Software_Solutions.Services
+{
+    public class EmailServices
+    {
+        private readonly IConfiguration _config;
+
+        public EmailServices(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string htmlContent)
+        {
+            var email = new MimeMessage();// Email message
+
+            email.From.Add(new MailboxAddress(_config["MailSettings:SenderName"],_config["MailSettings:SenderEmail"]));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = htmlContent };
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_config["MailSettings:SmtpHost"], 587 , SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_config["MailSettings:UserName"], _config["MailSettings:SmtpPassword"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
