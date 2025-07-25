7d134038c9280059246ad577333fa980f69d9e2a
The Engineer <132500641+OmarAyman2030@users.noreply.github.com>
2025-07-25 16:38:55 +0300

    Add files via upload

---
+++ Controllers/ServiceCont.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Software_Solutions.DTO;
+using Software_Solutions.Interface;
+
+namespace Software_Solutions.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ServiceCont : ControllerBase
+    {
+        private readonly IService _service;
+
+        public ServiceCont(IService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var services = await _service.GetAllServices();
+            return Ok(services);
+
+        }
+
+        [HttpGet("id")]
+         public async Task<ActionResult> GetByID(int id)
+         {
+            var service = await _service.GetServiceById(id);
+            if (service == null) return NotFound();
+
+            return Ok(service);
+
+         }
+
+        [Authorize (Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult> CreateService(ServiceDto dto)
+        {
+            var NewService = await _service.CreateService(dto);
+            return CreatedAtAction(nameof (GetByID), new {id = NewService.Id} , NewService);
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult> UpdateService(int id , ServiceDto dto)
+        {
+            var updated = await _service.UpdateService(id,dto);
+
+            if(!updated) return NotFound();
+
+            return NoContent();
+
+        }
+
+        [Authorize (Roles = "Admin")]
+        [HttpDelete]
+        public async Task<ActionResult> Delete (int id)
+        {
+            var deleted = await _service.DeleteService(id);
+            if(!deleted) return NotFound();
+
+            return NoContent();
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Software_Solutions.Data;
+using System.Security.Claims;
+
+namespace Software_Solutions.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UserCont : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserCont (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("profile-data")]
+        public async Task<ActionResult> GetProfile()
+        {
+            var userID = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userID == null) return Unauthorized("Token is invalid");
+
+            var user = await _context.Users.FindAsync(int.Parse(userID));
+
+            if (user == null) return Unauthorized("User Not found.");
+
+            var ProfileUser = new
+            {
+                user.ID,
+                user.FullName,
+                user.Email,
+                user.PhoneNumber
+            };
+
+            return Ok(new
+            {
+                message = $"Hello Eng/{user.FullName.Split(' ')[0]}",
+                ProfileUser
+            });
+
+        }
+    }
+}
