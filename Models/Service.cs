525f19f6ec8a6db1fc7485bff30f35258cbaa936
The Engineer <132500641+OmarAyman2030@users.noreply.github.com>
2025-07-25 16:45:14 +0300

    Add files via upload

---
+++ Models/Service.cs
@@ -0,0 +1,15 @@
+﻿namespace Software_Solutions.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string EstimatedTime { get; set; }
+        public string Category { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+    }
+}
